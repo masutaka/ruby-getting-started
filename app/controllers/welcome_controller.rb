a788fcde64e0cf90c5aeb99827f3ea41b890dc73
Takashi MASUDA <masutaka.net@gmail.com>
2020-01-22 18:08:14 +0900

    Add Custom counting of Librato

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,9 @@ class WelcomeController < ApplicationController
 
   # GET /welcome
   def index
-
+    $stdout.puts("count#user.clicks=1")
+    $stdout.puts("count#user.clicks=1")
+    $stdout.puts("count#user.clicks=3")
   end
 
 end
