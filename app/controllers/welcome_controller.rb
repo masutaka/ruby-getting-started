ddbaccdc32db0012233d2fbe177ed743deafeb55
Takashi MASUDA <masutaka.net@gmail.com>
2020-01-22 18:59:34 +0900

    tmp

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,6 +5,7 @@ class WelcomeController < ApplicationController
     Rails.logger.info("count#user.clicks=2")
     Rails.logger.info("count#user.clicks=2")
     Rails.logger.info("count#user.clicks=5")
+    Rails.logger.info("sample#queues_count.product_item_report=#{rand 20}")
   end
 
 end
